e1033bc119f7c930518c79ed125108f5da74ec0d
Reezon7 <therezon777@gmail.com>
2023-04-01 23:37:31 +0400

    parallel tests

--- src/test/java/SmokeTests.java
+++ src/test/java/SmokeTests.java
@@ -5,10 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 import pages.AddCustomerPage;
 import pages.CustomersListPage;
 import pages.MainPage;
@@ -19,40 +16,38 @@ import java.util.stream.Collectors;
 
 @Epic("Smoke Tests")
 public class SmokeTests {
-    WebDriver driver;
-    MainPage mainPage;
-    AddCustomerPage addCustomerPage;
-    CustomersListPage customersListPage;
+
+    ChromeOptions options;
 
     @BeforeClass
-    public void setup() {
-        ChromeOptions options = new ChromeOptions();
+    public void setOptions(){
+        options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
-        driver = new ChromeDriver(options);
-
-        mainPage = new MainPage(driver);
-        addCustomerPage = new AddCustomerPage(driver);
-        customersListPage = new CustomersListPage(driver);
     }
 
-    @BeforeMethod
-    public void openStartPage() {
+    public WebDriver setup(){
+        WebDriver driver = new ChromeDriver(options);
         driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/manager");
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().timeouts().implicitlyWait(Duration.ofMinutes(2));
+        return driver;
     }
 
     @Test
     @Description("Создание клиента")
     public void addCustomerTest() {
+        WebDriver driver = setup();
+        TestPages pages = new TestPages(driver);
         String firstName = "Test";
         String lastName = "Test";
         String postCode = "Test";
 
-        mainPage.addCustomer();
-        addCustomerPage.fillOutForm(firstName, lastName, postCode);
-        addCustomerPage.submitForm();
-        String alertMessage = addCustomerPage.getAlertMessage();
+        pages.mainPage.addCustomer();
+        pages.addCustomerPage.fillOutForm(firstName, lastName, postCode);
+        pages.addCustomerPage.submitForm();
+        String alertMessage = pages.addCustomerPage.getAlertMessage();
+
+        driver.quit();
 
         Assert.assertEquals("Customer added successfully with customer id :",
                 alertMessage.split("\\d")[0]);
@@ -61,14 +56,17 @@ public class SmokeTests {
     @Test
     @Description("Сортировка клиентов по имени")
     public void sortCustomersByFirstNameTest() {
-        mainPage.customers();
+        WebDriver driver = setup();
+        TestPages pages = new TestPages(driver);
 
-        List<String> firstNames = customersListPage.getFirstNames();
-        customersListPage.sortByFirstName();
-        customersListPage.sortByFirstName();
-        List<String> sortedFirstNames = customersListPage.getFirstNames();
-
+        pages.mainPage.customers();
+        List<String> firstNames = pages.customersListPage.getFirstNames();
+        pages.customersListPage.sortByFirstName();
+        pages.customersListPage.sortByFirstName();
+        List<String> sortedFirstNames = pages.customersListPage.getFirstNames();
         firstNames = firstNames.stream().sorted(String.CASE_INSENSITIVE_ORDER).collect(Collectors.toList());
+
+        driver.quit();
 
         Assert.assertEquals(firstNames.toArray(), sortedFirstNames.toArray());
     }
@@ -76,10 +74,15 @@ public class SmokeTests {
     @Test
     @Description("Поиск клиента по имени")
     public void searchCustomerByFirstNameTest() {
+        WebDriver driver = setup();
+        TestPages pages = new TestPages(driver);
         String firstName = "Harry";
-        mainPage.customers();
-        customersListPage.enterSearchTerm(firstName);
-        int size = customersListPage.getFirstNames().size();
+
+        pages.mainPage.customers();
+        pages.customersListPage.enterSearchTerm(firstName);
+        int size = pages.customersListPage.getFirstNames().size();
+
+        driver.quit();
 
         Assert.assertTrue(size > 0);
     }
@@ -87,10 +90,15 @@ public class SmokeTests {
     @Test
     @Description("Поиск клиента по фамилии")
     public void searchCustomerByLastNameTest(){
+        WebDriver driver = setup();
+        TestPages pages = new TestPages(driver);
         String lastName = "Potter";
-        mainPage.customers();
-        customersListPage.enterSearchTerm(lastName);
-        int size = customersListPage.getFirstNames().size();
+
+        pages.mainPage.customers();
+        pages.customersListPage.enterSearchTerm(lastName);
+        int size = pages.customersListPage.getFirstNames().size();
+
+        driver.quit();
 
         Assert.assertTrue(size > 0);
     }
@@ -98,10 +106,15 @@ public class SmokeTests {
     @Test
     @Description("Поиск клиента по почтовому индексу")
     public void searchCustomerByPostCodeTest(){
+        WebDriver driver = setup();
+        TestPages pages = new TestPages(driver);
         String postCode = "E725JB";
-        mainPage.customers();
-        customersListPage.enterSearchTerm(postCode);
-        int size = customersListPage.getFirstNames().size();
+
+        pages.mainPage.customers();
+        pages.customersListPage.enterSearchTerm(postCode);
+        int size = pages.customersListPage.getFirstNames().size();
+
+        driver.quit();
 
         Assert.assertTrue(size > 0);
     }
@@ -109,16 +122,16 @@ public class SmokeTests {
     @Test
     @Description("Поиск клиента по номеру аккаунта")
     public void searchCustomerByAccountNumberTest(){
+        WebDriver driver = setup();
+        TestPages pages = new TestPages(driver);
         String accountNumber = "1004";
-        mainPage.customers();
-        customersListPage.enterSearchTerm(accountNumber);
-        int size = customersListPage.getFirstNames().size();
+
+        pages.mainPage.customers();
+        pages.customersListPage.enterSearchTerm(accountNumber);
+        int size = pages.customersListPage.getFirstNames().size();
+
+        driver.quit();
 
         Assert.assertTrue(size > 0);
-    }
-
-    @AfterClass
-    public void quit() {
-        driver.quit();
     }
 }
